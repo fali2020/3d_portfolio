b291ef792a7b7020bc848b5c375745bad527ddbf
fali2020 <fatimahali01@hotmail.com>
2023-11-16 23:04:33 -0500

    build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactSWC from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactSWC()],
+  plugins: [react()],
   esbuild: {
     jsxFactory: 'React.createElement',
     jsxInject: 'import React from "react"',
